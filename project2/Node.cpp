b48fff9c94cfd5110cd5c4beff4c205c2104eecb
unknown <devinbrewer2000@gmail.com>
2018-11-27 14:20:29 -0700

    Outlined functions

---
+++ project2/Node.cpp
@@ -0,0 +1,22 @@
+#include "Node.h"
+
+// Constructor
+Node::Node(string parentID) {
+
+}
+
+void Node::updateRightHash(string RHASH) {
+  
+}
+
+void Node::updateLeftHash(string LHASH) {
+
+}
+
+void Node::setEvent(string rawEvent) {
+
+}
+
+void Node::setID(string ID) {
+
+}
---
+++ project2/TrackingTree.cpp
@@ -0,0 +1,18 @@
+#include "TrackingTree.h"
+
+// Constructor
+TrackingTree::TrackingTree() {
+  
+}
+
+void TrackingTree::newLayer() {
+
+}
+
+void TrackingTree::updateNode(string ID, string rawEvent) {
+
+}
+
+void TrackingTree::updateParent() {
+
+}
